087f3889ff85b1d7bb2eaf52f71132cc6ee36d29
Bekar <bekar.shekiladze@leverx.com>
2026-03-08 16:35:24 +0400

    fix: avoid further bug in formatting value amount

--- src/shared/utils/formatters/formatPrice.js
+++ src/shared/utils/formatters/formatPrice.js
@@ -2,5 +2,5 @@ export const formatPrice = (price, locale = "en-US", currency = "USD") => {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency,
-  }).format(price / 100);
+  }).format((price / 100).toFixed(2));
 };
